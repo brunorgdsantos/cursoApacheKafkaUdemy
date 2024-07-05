a8d7f849a3bb770ff13abf5131a9eb2caa3ea66b
Bruno Santos <brunorgdsantos@gmail.com>
2024-07-04 23:44:47 -0300

    primeiro commit

---
+++ fafkaDemo/src/main/java/org/example/EnvioKafka.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class EnvioKafka {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer(props);
+
+        ProducerRecord<String, String> record = new ProducerRecord("testeJava", "OlaMundo");
+        producer.send(record);
+        producer.close();
+    }
+}
